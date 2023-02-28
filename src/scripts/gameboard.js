39f00cc8f59c4f857da6ce7570866ccbe0b0fdf6
Tony W <bobacrypto2@gmail.com>
2023-02-27 22:08:44 -0800

    Create Gameboard function to create a board of 10x10 indexes ranging from [1, 1] to [10, 10]

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,10 +1,33 @@
 import {Ship} from './main';
 
 export class Gameboard {
-    constructor(ship) {
-        this.ship = ship;
+    constructor() {
+        this.board = this.createBoard();
         this.hit = 0;
         this.miss = 0;
+    }
+
+    createBoard() {
+        let boardArr = [];
+
+        let x = 1;
+        let y = 1;
+
+        while(y <= 10 && x <= 10) {
+            if(x == 10 && y == 10) {
+                boardArr.push([x, y])
+                return boardArr;
+            }
+
+            if (y == 10) {
+                boardArr.push([x, y])
+                x++
+                y = 1;
+            }
+
+            boardArr.push([x, y]);
+            y++
+        }
     }
 
     placeShip(x, dir) {
@@ -12,13 +35,11 @@ export class Gameboard {
         if (x < 1 || x > 10 || !directions.includes(dir)) {
             return;
         }
-
         // Place ship at x position laid out towards ( dir )
         // e.g place ship at 4 [ up, left, right, down ]
 
         // If down or up then take y and increment or decrement by 1 of each ship.length
         // if right or left then take y and increment or decrement the value by 1 of each ship.length
-
         const position = [x, dir];
         return position;
     }
@@ -26,11 +47,20 @@ export class Gameboard {
     receiveAttack(cord) {
         const cordMatch = (val) => cord.includes(val);
 
+        // Will have to change these parameters to an actual attack
+        // This assumes we already have a ship in place on the board
+        // Which means we will have to test if the ( cord ) matches an existing ship on [x, y] cordinates
+        // Need a way to track Gameboard and Ships on said Gameboard.
+
         if(this.placeShip(4, "up").every(cordMatch)) {
             return "hit";
         }
-        
+
         this.miss += 1;
         return "miss";
     }
 }
+
+const test = new Gameboard();
+
+console.log(test)
